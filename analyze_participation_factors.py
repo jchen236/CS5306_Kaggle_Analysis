545356cfaff8a4ea98bc3afe0d3ad542763ba9f1
Jimmy Chen <jzc8@cornell.edu>
2018-10-02 17:40:14 -0400

    Calculate reward vs team and team submissions

--- analyze_participation_factors.py
+++ analyze_participation_factors.py
@@ -1,1 +1,107 @@
 import urllib.request, json
+import matplotlib.pyplot as plt
+
+# The goal of this file is to analyze metadata about a competition and see how that affects the number of teams
+# that submit as well as the total number of submissions
+
+
+# $reward vs # of teams
+# $reward vs # of submissions
+# awardsPoints vs # of teams
+# awardsPoints vs # of submissions
+# categories vs # of teams
+# categories vs # of submissions
+
+#### LOAD JSON ####
+competition_data = []
+leaderboard_data = []
+with open('kaggle_competition_data.json') as competition_json_data:
+    competition_data = json.load(competition_json_data)
+with open('kaggle_competition_leaderboard_data.json') as leaderboard_json_data:
+    leaderboard_data = json.load(leaderboard_json_data)
+
+## CALCULATE REWARD VS TEAMS/SUBMISSIONS AND REWARD TYPE VS TEAMS/SUBMISSIONS
+reward_to_teams = []
+reward_to_submissions = []
+reward_type_to_teams = {}
+reward_type_to_submissions = {}
+num_competitions = 0
+for i in range(1, 16):
+    competition_page = competition_data['{}'.format(i)]
+    competitions = (competition_page['pagedCompetitionGroup']['competitions'])
+    for competition in competitions:
+        num_competitions+=1
+        competition_id = competition['competitionId']
+        if competition_id == "8587":
+            print( reward_quantity = competition['rewardQuantity'])
+        # print("Processing competition {}".format(competition_id))
+        # print("\n")
+        reward_type_name = competition['rewardTypeName']
+        reward_quantity = competition['rewardQuantity']
+        if reward_type_name == None:
+            print("NO REWARD TYPE NAME  ")
+            print(competition_id)
+        total_teams = competition['totalTeams']
+        leaderboard_submissions_for_competition = leaderboard_data[str(competition_id)]
+        total_submissions = 0
+
+        if leaderboard_submissions_for_competition == []:
+            continue;
+        for submission in leaderboard_submissions_for_competition['submissions']:
+            total_submissions += int((submission['entries']))
+
+        # Populate lists and dictionaries
+        reward_to_teams.append( (reward_quantity if (reward_quantity != None) else 0, total_teams))
+        reward_to_submissions.append( (reward_quantity if (reward_quantity != None) else 0, total_submissions))
+
+        if reward_type_name in reward_type_to_teams:
+            reward_type_to_teams[reward_type_name].append(total_teams)
+        else:
+            reward_type_to_teams[reward_type_name] = [total_teams]
+
+        if reward_type_name in reward_type_to_submissions:
+            reward_type_to_submissions[reward_type_name].append(total_submissions)
+        else:
+            reward_type_to_submissions[reward_type_name] = [total_submissions]
+
+print(reward_to_teams)
+print("*"*80)
+print(reward_to_submissions)
+print("*"*80)
+print(reward_type_to_teams)
+print("*"*80)
+print(reward_type_to_submissions)
+
+
+# show relationship between reward to number of teams
+# x = []
+# y = []
+# for pair in reward_to_teams:
+#     x.append(pair[0] / 1000.0)
+#     y.append(pair[1])
+
+# plt.scatter(x,y)
+# plt.xlabel('Award amount in thousands of $')
+# plt.ylabel('Number of Teams per Competition')
+# plt.title("Effect of Award Amount on Number of Teams")
+# plt.savefig("AwardVsTeamCount.png")
+# plt.show()
+
+
+# show relationship between reward to number of submissions
+x = []
+y = []
+for pair in reward_to_submissions:
+    x.append(pair[0] / 1000.0)
+    y.append(pair[1])
+
+plt.scatter(x,y)
+plt.xlabel('Award amount in thousands of $')
+plt.ylabel('Number of Submissions per Competition')
+plt.title("Effect of Award Amount on Number of Submissions")
+plt.savefig("AwardVsSubmission.png")
+plt.show()
+
+
+        
+
